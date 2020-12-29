ea018a03c6dc7bfaf8f882e11874c236afd7bf2a
Debora Goncalves <Debora.Goncalves@senior.com.br>
2020-12-29 08:55:07 -0300

    insert - staff e pontos por jogo

--- insert-data.sql
+++ insert-data.sql
@@ -1,1 +1,11 @@
 INSERT INTO players (name, number, position) VALUES ('Jaqueline', 8, 'Ponteira');
+
+INSERT INTO players (name, number, position) VALUES ('Jaqueline', 8, 'Ponteira');
+
+INSERT INTO staff (name, function) VALUES ('Luizomar', 'Coach');
+
+INSERT INTO staff (name, function) VALUES ('Luizomar', 'Coach');
+
+INSERT INTO pointsGame (id_player, number_points, game, season, championship) VALUES (1, 10, 'Osasco x São Caetano', '2019/2020', 'Superliga');
+
+INSERT INTO pointsGame (id_player, number_points, game, season, championship) VALUES (1, 20, 'Osasco x Rio', '2019/2020', 'Superliga');
