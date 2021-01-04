c706138b432167a6613554a3a35239189d58e9de
Debora Goncalves <Debora.Goncalves@senior.com.br>
2021-01-04 09:53:18 -0300

    Group by e order by

---
+++ count/count-group-by.sql
@@ -0,0 +1,3 @@
+-- COUNT usando GROUP BY para dois valores
+
+SELECT COUNT(id_player) FROM players GROUP BY number;
---
+++ order-by/order-by-desc.sql
@@ -0,0 +1,3 @@
+-- ORDER BY: o padrão é ASC, se for DESC especificar.
+
+SELECT id_player FROM players ORDER BY id_player DESC;
