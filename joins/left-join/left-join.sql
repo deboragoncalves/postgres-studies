3fcafbd6fd6ce2f9597db2673b0130eb71f11253
Debora Goncalves <Debora.Goncalves@senior.com.br>
2021-01-04 13:33:58 -0300

    Left e right join

---
+++ joins/left-join/left-join.sql
@@ -0,0 +1,4 @@
+-- LEFT JOIN: jogadoras x numero de pontos. exibe todos os dados da tabela de jogadores (esquerda), independente se estão 
+-- relacionados com a coluna da direita (pontos por jogo).
+
+SELECT players.id_player, players.name FROM players LEFT JOIN pointsgame ON pointsgame.id_player = players.id_player;
